47f591e79a6a91f095c339e633caea95f11eaef4
Dereit <106438546+Dereit@users.noreply.github.com>
2025-09-13 18:12:41 +0500

    Task2

---
+++ TestProject1/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.GroshevID.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.GroshevID.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Илья";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет..., Илья", res);
+        }
+    }
+}
---
+++ Tyuiu.GroshevID.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+using System;
+
+using Tyuiu.GroshevID.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.GroshevID.Sprint0.Task2.V0.Test
+{
+    public class DataServiceTest
+    {
+        [Fact]
+        public void CheckGetMessageValid()
+        {
+            var name = "Илья";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Илья", res);
+        }
+    }
+}
