6c17430c5b9738cd196218bf33977361c5312003
Dereit <106438546+Dereit@users.noreply.github.com>
2025-09-16 10:29:19 +0500

    Task4

---
+++ Tyuiu.GroshevID.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.GroshevID.Sprint0.Task4.V0.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.Addition(1, 5));
+        Console.WriteLine(DataService.Subtraction(15, 5));
+        Console.WriteLine(DataService.Multiplication(10, 10));
+        Console.WriteLine(DataService.Division(5, 5));
+
+        Console.ReadKey();
+    }
+}
