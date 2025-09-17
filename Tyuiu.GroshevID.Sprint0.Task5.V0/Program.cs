5e72802fd5fab24f9415111d556f4e9dd52bb621
Dereit <106438546+Dereit@users.noreply.github.com>
2025-09-17 09:39:42 +0500

    Task5

---
+++ Tyuiu.GroshevID.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.GroshevID.Sprint0.Task5.V0.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("A + B = " + DataService.Addition(1, 5));
+        Console.WriteLine("A - B = " + DataService.Subtraction(15, 5));
+        Console.WriteLine("A * B = " + DataService.Multiplication(10, 10));
+
+        Console.WriteLine("A / B = " + DataService.Division(9, 3));
+
+        Console.ReadKey();
+    }
+}
