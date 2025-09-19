59beec96a2d64cd89e454d2d7b10919178de8449
Dereit <106438546+Dereit@users.noreply.github.com>
2025-09-19 09:49:31 +0500

    Task7

---
+++ Tyuiu.GroshevID.Sprint0.Task7.V0/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.GroshevID.Sprint0.Task7.V0.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Title = "Спринт #0 | Выполнил: Грошев И. Д. | РППб-25-1";
+
+        Console.WriteLine("ОПИСАНИЕ ПРОЕКТА");
+
+        int[] arrayNums1 = new int[] { 1, 2, 3, 4, 5 };
+        Console.WriteLine("Значение элементов массива №1");
+        for (int i = 0; i < arrayNums1.Length; i++)
+        {
+            Console.Write(arrayNums1[i] + ", ");
+        }
+
+        Console.WriteLine();
+
+        int[] arrayNums2 = new int[] { 1, 2, 3, 4, 5 };
+        Console.WriteLine("Значение элементов массива №2");
+        for (int i = 0; i < arrayNums2.Length; i++)
+        {
+            Console.Write(arrayNums2[i] + ", ");
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("ОПИСАНИЕ ПРОЕКТА");
+        if (arrayNums1.Length == arrayNums2.Length)
+        {
+            int[] resultArray = DataService.AdditionArrays(arrayNums1, arrayNums2);
+
+            Console.WriteLine("Сумма элементов массива равна:");
+            for (int i = 0; i < resultArray.Length; i++)
+            {
+                Console.Write(resultArray[i] + ", ");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Ошибка! Исходные имеют разное количество элементов");
+        }
+        Console.ReadKey();
+    }
+}
